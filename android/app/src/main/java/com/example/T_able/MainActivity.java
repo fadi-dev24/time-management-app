899b16ece713535555f42e5824b06f1090152041
fadi-dev24 <117908849+fadi-dev24@users.noreply.github.com>
2022-11-10 04:08:54 -0800

    Add files via upload

---
+++ android/app/src/main/java/com/example/T_able/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.T_able;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SmartProductivityApp());
+}
+
+class SmartProductivityApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'T-able',
+      home: Container(),
+    );
+  }
+}
